37b66c35ebcb67d074e63bf19af865e7ea52d650
Emmanuel Amblard <prof.amblard@lilo.org>
2025-04-08 20:08:32 +0200

    Remise du travail en fin de tuto v2

--- Remise-Travail-Fin-Tuto.bat
+++ Remise-Travail-Fin-Tuto.bat
@@ -9,6 +9,16 @@ SET    PATH_EXE_7ZIP=%PATH_ROOT%\utils\bin\7z.exe
 
 SET      NOM_EI=Tuto Web Java
 
+:: Zaclys
+SET    URL_VISU_1=https://acloud10.zaclys.com/index.php/s/fTJH4jPoCTc9Y7c
+SET TOKEN_DEPOT_1=CHba7DyDSorXgTE
+SET    URL_VISU_2=https://acloud10.zaclys.com/index.php/s/NG6Cp8JxNQLqTfZ
+SET TOKEN_DEPOT_2=Fo9EQRpNRDmGTFj
+SET    URL_VISU_3=https://acloud10.zaclys.com/index.php/s/p4eLnjJAz6M8jQ7
+SET TOKEN_DEPOT_3=BpJzDcxLjkn24ys
+SET     URL_DEPOT=https://acloud10.zaclys.com
+
+:: Libretic
 SET    URL_VISU_1=https://nextcloud.libretic.fr/s/L99fSW2FkniYpPs
 SET TOKEN_DEPOT_1=yqsS7FZDWB3Te72
 SET    URL_VISU_2=https://nextcloud.libretic.fr/s/ijQDMKL2D8R9PCJ
@@ -16,7 +26,7 @@ SET TOKEN_DEPOT_2=679fpTS8Bpqc742
 SET    URL_VISU_3=https://nextcloud.libretic.fr/s/C7WnRC5sDXWPkCP
 SET TOKEN_DEPOT_3=nciPEoqCXFzTBbR
 SET     URL_DEPOT=https://nextcloud.libretic.fr
-
+	
 
 :: Affiche le nom de l'EI
 ECHO.
@@ -25,7 +35,7 @@ ECHO.
 
 :saisie-groupe
 ECHO.
-SET ^GROUPE=
+SET GROUPE=
 SET /P GROUPE=Quel est votre groupe de TD (1, 2, 3 ou 4) : 
 IF "%GROUPE%" == "1" (
   SET    URL_VISU=%URL_VISU_1%
@@ -75,9 +85,38 @@ IF EXIST "%PATH_ARCHIVE%" DEL "%PATH_ARCHIVE%"
 "%PATH_EXE_7ZIP%" %ARGS_7Z%
 
 :: Envoie le fichier
+ECHO.
 CALL :formaURL URL_DEST "%URL_DEPOT%/public.php/webdav/%FILE_ARCHIVE%"
-curl -T "%PATH_ARCHIVE%" -u "%TOKEN_DEPOT%":"" "%URL_DEST%"
+::curl -T "%PATH_ARCHIVE%" -u "%TOKEN_DEPOT%":"" "%URL_DEST%"
 
+::EcHO. & PAUSE
+::goto :eof
+
+SET count=0
+FOR /F "tokens=* USEBACKQ" %%F IN (`curl --version`) DO (
+  CALL SET /A count=%%count%% + 1
+)
+IF "%count%" == "0" (
+  ECHO   ┌─────────────────────┐
+  ECHO   │ Echec du transfert  │
+  ECHO   └─────────────────────┘ 
+  ECHO. & PAUSE
+  GOTO :EOF
+)
+
+SET count=0
+FOR /F "tokens=* USEBACKQ" %%F IN (`curl -T "%PATH_ARCHIVE%" -u "%TOKEN_DEPOT%":"" "%URL_DEST%"`) DO (
+  CALL SET /A count=%%count%% + 1
+  ECHO %%F
+)
+IF NOT errorlevel 1 IF "%count%" == "0" GOTO :open-visu
+ECHO   ┌─────────────────────┐
+ECHO   │ Echec du transfert  │
+ECHO   └─────────────────────┘ 
+ECHO. & PAUSE
+GOTO :EOF
+
+:open-visu
 :: Ouvre la page de vérification visuelle
 START "" "%URL_VISU%"
 
