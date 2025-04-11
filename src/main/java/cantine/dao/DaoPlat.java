e16bab0f682569bd6c757e1682ad19ae58ae33e9
lemofack <lemofack@3il.fr>
2025-04-11 08:54:20 +0200

    tuto7

--- src/main/java/cantine/dao/DaoPlat.java
+++ src/main/java/cantine/dao/DaoPlat.java
@@ -1,10 +1,18 @@
 package cantine.dao;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.lang.NonNull;
 
 import cantine.data.Plat;
+import cantine.data.TypePlat;
 
-public interface DaoPlat extends CrudRepository<Plat, Long>, PagingAndSortingRepository<Plat, Long> {
+public interface DaoPlat extends CrudRepository<TypePlat, Long> {
+
+   
+    @Query("SELECT * FROM ...")
+    @NonNull
+    Iterable<TypePlat> findAll();
+
 	
 }
